c48af60e1183a0211830f5855cfe3fe6ef38a0fc
NandiniAggarwal14 <152698318+NandiniAggarwal14@users.noreply.github.com>
2024-05-05 18:37:58 +0530

    Update reverse_array_particular_index_dma.c

--- reverse_array_particular_index_dma.c
+++ reverse_array_particular_index_dma.c
@@ -1,29 +1,41 @@
-//Reverse array elements from a particular index to last using dynamic memory allocation
+/* 
+NAME                                  : NANDINI AGGARWAL
+UNIVERSITY ROLL NO. : 2023893
+ROLL NO.                           : 50
+SECTION                             : F
+
+8. Write a Program to input an array and reverse the array elements from particular index to the 
+last using dynamic memory allocation.
+
+*/
+
 #include<stdio.h>
 #include<stdlib.h>
 int main()
 {
-int i,n,*p,index,j,tmp;
-printf("\nInput the number of array elements: ");
-scanf("%d",&n);
-p=(int *)malloc(n*sizeof(int));
-printf("\nInput the array elements: \n");
-for(i=0;i<n;i++)
-{
-scanf("%d",(p+i));
-}
-printf("\nInput the particular index from where the array is to be reversed: \n");
-scanf("%d",&index);
-for(i=index, j=n-1; i<j; i++, j--)
-{
-tmp=*(p+j);
-*(p+j)=*(p+i);
-*(p+i)=tmp;
-}
-printf("\nThe array after reversing is:\n");
-for(i=0;i<n;i++)
-{
-printf("%d\n",*(p+i));
-}
-return 0;
+    int i,n,*p,index,j,tmp;
+    printf("\t\t\t\t\t**********INPUT**********\n\n");
+    printf("\nInput the number of array elements: ");
+    scanf("%d",&n);
+    p=(int *)malloc(n*sizeof(int));
+    printf("\nInput the array elements: \n");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",(p+i));
+    }
+    printf("\nInput the particular index from where the array is to be reversed: \n");
+    scanf("%d",&index);
+    for(i=index, j=n-1; i<j; i++, j--)
+    {
+        tmp=*(p+j);
+        *(p+j)=*(p+i);
+        *(p+i)=tmp;
+    }        
+    printf("\t\t\t\t\t**********OUTPUT**********\n\n");
+    printf("\nThe array after reversing is:\n");
+    for(i=0;i<n;i++)
+    {
+        printf("%d\n",*(p+i));
+    }
+    return 0;
 }
